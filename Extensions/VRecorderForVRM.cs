24db2efe46eda910a9cade77655be9dea9da19bb
Yokoyama Yuto <onizakura8180@docomo.ne.jp>
2019-06-04 15:29:34 +0900

    拡張機能:VRMへの対応 VRMBlendShapeProxyの値を記録

---
+++ Extensions/VRecorderForVRM.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using VRM;
+
+namespace VRec.Extensions
+{
+    public class VRecorderForVRM : VRec.VRecorderScript
+    {
+        private void Start()
+        {
+            AvatarRecordHandler += GetBlendShapes;
+        }
+
+        private VRecEventData[] GetBlendShapes(VRecRecordObj obj)
+        {
+            var data = new VRecEventData()
+            {
+                ObjectId = obj.Id,
+                Label = obj.Obj.name,
+                Type = ObjType.Avatar,
+                StrData = new string[1] { "BlendShape" },
+            };
+            var blends = obj.Obj.GetComponent<VRMBlendShapeProxy>();
+
+            if (blends != null)
+            {
+                var blendValues = new List<float>();
+                foreach (BlendShapePreset t in Enum.GetValues(typeof(BlendShapePreset)))
+                {
+                    var blendValue = blends.GetValue(t);
+                    blendValues.Add(blendValue);
+                }
+                data.FloatValues = blendValues.ToArray();
+            }
+
+            return new VRecEventData[1] { data };
+        }
+    }
+}
---
+++ Extensions/VViewerForVRM.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using VRM;
+namespace VRec.Extensions
+{
+    public class VViewerForVRM : VRec.VViewerScript
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+            AvatarDataHandler += BlendShapeSetting;
+        }
+
+        private void BlendShapeSetting(VRecRecordObj obj, VRecEventData data)
+        {
+            if (data.StrData[0] == "BlendShape")
+            {
+                var blend = obj.Obj.GetComponent<VRMBlendShapeProxy>();
+                if (!blend)
+                {
+                    return;
+                }
+                var blendNumber = 0;
+                foreach (BlendShapePreset t in Enum.GetValues(typeof(BlendShapePreset)))
+                {
+                    blend.SetValue(t, data.FloatValues[blendNumber]);
+                    blendNumber++;
+                }
+            }
+        }
+    }
+}
